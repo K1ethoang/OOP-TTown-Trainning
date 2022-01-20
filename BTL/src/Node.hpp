862b8c1dd28ea900ba4a77996186f8a54b81374f
Hoang Gia Kiet <kiethoang101@gmail.com>
2022-01-20 22:48:12 +0700

    Add: class Queue, Stack, Node

---
+++ BTL/src/Node.hpp
@@ -0,0 +1,19 @@
+#pragma once
+template <class T>
+class Node
+{
+public:
+    T data;
+    Node<T> *pNext;
+
+public:
+    Node();
+    Node(T value);
+};
+
+template <class T>
+Node<T>::Node() {}
+
+template <class T>
+Node<T>::Node(T value)
+    : data(value), pNext(NULL) {}
