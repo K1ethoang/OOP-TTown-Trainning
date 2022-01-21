3dcbd5c59c7dc217334a7f0a143d1b6167c11407
Kiet Hoang <88199151+K1ethoang@users.noreply.github.com>
2022-01-21 21:35:12 +0700

    Update: comment

--- BTL/src/main.cpp
+++ BTL/src/main.cpp
@@ -1,3 +1,15 @@
+// #############################################################################
+// ###                     © Copyright by K1ethoang                          ###
+// #############################################################################
+// ###           Name: Hoang Gia Kiet                                        ###
+// ###           Class: CNTT - K62                                           ###
+// ###           Tool: VSCode                                                ###
+// ###           BM CNTT - DH GIAO THONG VAN TAI - PHAN HIEU TP.HCM          ###
+// #############################################################################
+// ### Github: https://github.com/K1ethoang                                  ###
+// ### Youtube: https://www.youtube.com/channel/UCK5Ey9NoSyo7PYBmM3OvjtQ     ###
+// #############################################################################
+
 #include "menu.hpp"
 
 int main()
